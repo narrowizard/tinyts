167bc3cbdafc97f6e56944273ea12567f3608d77
wangc <wangc@jiudeng.me>
2016-05-31 14:13:19 +0800

    修改文件大小写

--- controls/ulList.ts
+++
@@ -1,5 +0,0 @@
-import {ListView} from '../core/ListView';
-
-export class UlList<T extends IModel> extends ListView<T> {
-    
-}
--- third-party/array.d.ts
+++
@@ -1,4 +0,0 @@
-interface Array<T> {
-    remove(index: number);
-    remove(predicate: (elem: T) => boolean);
-}
