4bdc922937ff0a8a4803ec094318512e2b2192a4
Narro <wangc@jiudeng.me>
2016-06-29 14:09:01 +0800

    修复virtualView多次绑定的bug

--- core/Core.ts
+++ core/Core.ts
@@ -73,7 +73,7 @@ export abstract class VirtualView<T> extends View implements IViewModel {
     LoadView() {
         this.SetTemplate();
         super.LoadView();
-        this.target.append(this.template);
+        this.target.html(this.template);
 
         //在这里注入control
         inject(this.constructor, this);
