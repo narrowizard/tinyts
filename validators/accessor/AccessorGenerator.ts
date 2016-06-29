8e271364b7f88893e80385b0f051cd41ef7dbfa3
Narro <wangc@jiudeng.me>
2016-06-29 16:16:51 +0800

    基本完成模型验证器

--- validators/accessor/AccessorGenerator.ts
+++ validators/accessor/AccessorGenerator.ts
@@ -3,14 +3,14 @@ export function GenerateSetAccessor<T>(validator: (...args: T[]) => boolean, get
         var originalSet = descriptor.set;
         if (descriptor.set != null) {
             descriptor.set = function (...args: T[]) {
-                if (!validator(...args)) {
+                if (validator(...args)) {
                     //验证成功
                     originalSet.call(this, args);
                 } else {
                     if (!target["__tag__"]) {
                         target["__tag__"] = {};
                     }
-                    var temp = target["__tag__"][propertyName];
+                    var temp = target.constructor["__tag__"]["_" + propertyName];
                     var tagName = temp == null ? propertyName : temp;
                     //在这里调用错误回调
                     if (target.OnValidateError) {
--- validators/accessor/StringAccessor.ts
+++ validators/accessor/StringAccessor.ts
@@ -2,24 +2,32 @@ import {GenerateSetAccessor} from './AccessorGenerator';
 
 export function MaxLength(maxLength: number) {
     return GenerateSetAccessor<string>((value) => {
-        return value.length > maxLength;
+        return value.length <= maxLength;
     }, (tagName) => {
-        return `${tagName}最大长度为${maxLength}`
+        return `${tagName}最大长度为${maxLength}`;
     });
 }
 
 export function MinLength(minLength: number) {
     return GenerateSetAccessor<string>((value) => {
-        return value.length < minLength;
+        return value.length >= minLength;
     }, (tagName) => {
-        return `${tagName}最小长度为${minLength}`
+        return `${tagName}最小长度为${minLength}`;
     });
 }
 
 export function Required() {
     return GenerateSetAccessor<string>((value) => {
-        return value == "";
+        return value != "";
     }, (tagName) => {
-        return `${tagName}不能为空`
+        return `${tagName}不能为空`;
     });
+}
+
+export function Mobile() {
+    return GenerateSetAccessor<string>((value) => {
+        return /^1[3587]\d{9}$|^(0\d{2,3}-?|\(0\d{2,3}\))?[1-9]\d{4,7}(-\d{1,8})?$/.test(value);
+    }, (tagName) => {
+        return `${tagName}不是正确的手机号`;
+    })
 }
