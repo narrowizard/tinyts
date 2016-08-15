c8dcba3190e1549a45ed390efc55e3fd82a1a251
Narro <wangc@jiudeng.me>
2016-08-15 17:02:04 +0800

    服务修改

--- core/ServicePool.ts
+++ core/ServicePool.ts
@@ -42,7 +42,130 @@ class ServicePool {
 
     }
 
-
 }
 
 export var ServicePoolInstance = new ServicePool();
+
+/**
+ * stable 表示该方法可以使用数据池缓存数据
+ */
+function stable() {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        if (!target.__methods__) {
+            target.__methods__ = [];
+        }
+        target.__methods__.push(propertyKey);
+    }
+}
+
+interface IHttpRequest {
+
+    GET<T>(url: string, data?): Promise<T>;
+
+    POST<T>(url: string, data): Promise<T>;
+}
+
+/**
+ * 这是一个处理Http请求的类,在该类的底层实现了一个数据池
+ * 针对一些常用的且
+ */
+export class HttpRequset {
+
+    private static http: IHttpRequest;
+
+    /**
+     * SetHttpHandler 设置http处理器
+     */
+    static SetHttpHandler(handler: IHttpRequest) {
+        this.http = handler;
+    }
+
+    static Get<T>(url: string, data?): Promise<T> {
+        // 发送http请求
+        var promise = this.http.GET<T>(url, data);
+        promise.then((response) => {
+
+            return response;
+        })
+        return promise;
+    }
+}
+
+// 以下是测试内容
+
+// 这是一个实现http方法的类,在这里直接使用了jquery ajax
+// 你可以在这里实现对http response的统一处理
+class http implements IHttpRequest {
+
+    GET<T>(url: string, data?) {
+        var p = new Promise<T>((resolve, reject) => {
+            $.ajax({
+                url: url,
+                data: data,
+                type: "GET",
+                success: (response) => {
+                    if (response.Code == 0) {
+                        resolve(response.Data);
+                    } else {
+                        reject(response);
+                    }
+                },
+                error: (response) => {
+                    reject({ Code: -1, Message: "网络错误!" });
+                }
+            });
+        });
+        // 可以在这里统一处理错误
+        p.catch((response) => {
+            console.log(response.Message);
+        });
+
+        return p;
+    }
+
+    POST<T>(url: string, data?) {
+        var p = new Promise<T>((resolve, reject) => {
+            $.ajax({
+                url: url,
+                data: data,
+                type: "POST",
+                success: (response) => {
+                    if (response.Code == 0) {
+                        resolve(response.Data);
+                    } else {
+                        reject(response);
+                    }
+                },
+                error: (response) => {
+                    reject({ Code: -1, Message: "网络错误!" });
+                }
+            });
+        });
+
+        // 可以在这里统一处理错误
+        p.catch((response) => {
+            console.log(response.Message);
+        });
+
+        return p;
+    }
+}
+
+// 首先设置http处理器
+HttpRequset.SetHttpHandler(new http());
+
+// 下面实现一个service
+class DemoService {
+
+    @stable()
+    GetData(data: { Param1: string, Param2: number }) {
+        return HttpRequset.Get<string[]>("/program/getsomedata", data);
+    }
+}
+
+// 下面使用该service
+
+ServicePoolInstance.GetService(DemoService).GetData({ Param1: "我是第一个参数", Param2: 1 }).then((data) => {
+    // 输出返回值
+    console.log(data.join(","));
+});
