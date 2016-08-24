1c2d0404bf72ae1bfa2fc2c620a8e4bcd4258d1d
Narro <wangc@jiudeng.me>
2016-08-24 14:24:57 +0800

    jquery以amd模块导入

---
+++ core/tinyts.ts
@@ -0,0 +1,17 @@
+import {View} from './view';
+import * as $ from 'jquery';
+
+export class VmTc {
+    constructor() {
+        var v = new View();
+        $.ajax({});
+    }
+}
+
+export function v() {
+
+}
+
+export function p() {
+
+}
