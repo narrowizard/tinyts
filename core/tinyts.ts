05917b01e95ae0dcb71db5d430bc48ea5eb6a15e
Narro <wangc@jiudeng.me>
2016-08-24 16:55:32 +0800

    增加reflect

--- core/tinyts.ts
+++ core/tinyts.ts
@@ -1,11 +1,8 @@
 import {View} from './view';
-import {Promise} from 'promise';
 
 export class VmTc {
     constructor() {
-        var aa = new Promise<string>((resolve, reject) => {
 
-        });
     }
 }
 
