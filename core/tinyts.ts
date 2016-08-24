b83294f616f1adf7bafd3bcb686f468df1e79808
Narro <wangc@jiudeng.me>
2016-08-24 14:56:24 +0800

    增加jquery linq声明文件

--- core/tinyts.ts
+++ core/tinyts.ts
@@ -1,10 +1,12 @@
 import {View} from './view';
 import * as $ from 'jquery';
+import * as linq from 'linqjs';
 
 export class VmTc {
     constructor() {
         var v = new View();
         $.ajax({});
+        linq.from([1, 2, 3]).where(it => it == 1).firstOrDefault();
     }
 }
 
