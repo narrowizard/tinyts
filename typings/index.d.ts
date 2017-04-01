a575c928cd3e3e12b548c8e4d2df059bb0316450
narro <501xs05@163.com>
2017-04-01 17:30:06 +0800

    增加mustache

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/multiplexjs/index.d.ts" />
+/// <reference path="globals/mustache/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/jsdom/index.d.ts" />
 /// <reference path="modules/requirejs/index.d.ts" />
