b1b879287c7fe5e8f6ac360fe01be4be11e5b2ee
narro <501xs05@163.com>
2017-05-04 14:42:41 +0800

    添加分页器

--- control/list.ts
+++ control/list.ts
@@ -255,4 +255,253 @@ export class ListView<T> extends View {
         }
     }
 
+    //分页
+    protected pageManager: PageManager<T>;
+
+    /**
+     * GetPageManager 获取分页器,仅当data-pagable为sync或async时有效
+     */
+    GetPageManager(): PageManager<T> {
+        if (!this.pageManager) {
+            console.error("data-pagable has not defined!");
+        }
+        return this.pageManager;
+    }
+
+    /**
+     * SetPageSize 设置每页条数,显示到页面上
+     */
+    SetPageSize(pagesize: number) {
+
+    }
+
+    /**
+     * SetCurPage 设置当前页(用于展示)
+     */
+    SetCurPage(page: number) {
+
+    }
+
+    /**
+     * SetPageCount 设置总页数(用于展示)
+     */
+    SetPageCount(count: number) {
+
+    }
+
+    /**
+     * GetPageSize 获取每页条数
+     */
+    GetPageSize(): number {
+        return 0;
+    }
+
+}
+
+// PAGEMODE 分页模式
+// SYNC 同步分页
+// ASYNC 异步分页
+enum PAGEMODE { SYNC, ASYNC };
+
+class PageManager<T> {
+
+    /**
+     * mData 同步分页模式的数据(异步时无效)
+     */
+    protected mData: T[];
+
+    /**
+     * SetData 设置数据,当模式为同步分页模式时,可以直接调用该函数将数据交给PageManager
+     * @param data 数据,同时会更新total和pageCount
+     */
+    SetData(data: T[]) {
+        if (!data) {
+            return;
+        }
+        this.mData = data;
+        this.SetRecordCount(this.mData.length);
+    }
+
+    /**
+     * SetContext 设置上下文
+     * @param context 数据获取器
+     */
+    SetContext(context: ListView<T>) {
+        this.context = context;
+    }
+
+    protected context: ListView<T>;
+
+    /**
+     * @param instance 同步模式时,数据会被设置到该instance
+     */
+    constructor(instance?: ListView<T>) {
+        this.curPage = 1;
+    }
+
+    protected pageMode: PAGEMODE;
+
+    /**
+     * SetPageMode 设置分页模式
+     * 同步模式时,需要在构造函数中传入目标ListView的实例
+     * 异步模式时,需要设置用于获取异步数据的context
+     * @param mode 分页模式
+     */
+    SetPageMode(mode: PAGEMODE) {
+        this.pageMode = mode;
+    }
+
+    protected curPage: number;
+
+    /**
+     * SetCurPage 强行设置当前页,不跳转
+     * 注意,调用此函数会引起分页器的奔溃,请谨慎使用
+     * @param index 页码
+     */
+    ForceSetCurPage(index: number) {
+        this.curPage = index;
+    }
+
+    protected pageSize: number;
+
+    /**
+     * CurPage 获取当前页码
+     */
+    CurPage() {
+        return this.curPage;
+    }
+
+    /**
+     * SetPageSize 设置每页条数
+     * @param pagesize 每页条数
+     */
+    SetPageSize(pagesize: number) {
+        this.pageSize = pagesize;
+    }
+
+    protected total: number;
+
+    /**
+     * RecordCount 返回记录总条数,仅在通过SetRecordCount方法设置总条数后才有效
+     */
+    RecordCount(): number {
+        return this.total;
+    }
+
+    /**
+     * SetRecordCount 设置记录总条数,同时设置pageCount
+     * @param count 记录总数量
+     */
+    SetRecordCount(count: number) {
+        this.total = count;
+        this.SetPageCount(Math.ceil(this.total / this.pageSize));
+    }
+
+    protected pageCount: number;
+
+    /**
+     * SetPageCount 设置总页数
+     * @param count 总页数
+     */
+    SetPageCount(count: number) {
+        //在列表上展示总页数
+        this.context.SetPageCount(count);
+        this.pageCount = count;
+    }
+
+    /**
+     * ResetCurPage 重置当前页为第一页
+     */
+    ResetCurPage() {
+        this.curPage = 1;
+    }
+
+    /**
+     * GetCurPage 获取当前页的数据
+     */
+    GetCurPage() {
+        //在列表上展示当前页
+        this.context.SetCurPage(this.curPage);
+        //获取每页条数
+        var pagesize = this.context.GetPageSize();
+        if (!pagesize) {
+            console.error("pagesize is wrong!");
+            return;
+        }
+        this.SetPageSize(pagesize);
+
+        if (this.pageMode == PAGEMODE.SYNC) {
+            //同步分页模式,直接将数据交给ListView
+            this.context.SetData(mx(this.mData).skip((this.curPage - 1) * this.pageSize).take(this.pageSize).toArray());
+        } else if (this.pageMode == PAGEMODE.ASYNC) {
+            if (!this.context) {
+                throw "context has not been set!";
+            }
+            //异步分页模式,请求服务器
+            this.getData(this.curPage, this.pageSize);
+        }
+    }
+
+    /**
+     * getData 异步获取数据回调,异步模式时请设置此方法
+     * @param index 页码
+     * @param pagesize 每页条数
+     */
+    getData: (index: number, pagesize: number) => void;
+
+    /**
+     * FirstPage 首页
+     */
+    FirstPage() {
+        this.curPage = 1;
+        this.GetCurPage();
+    }
+
+    /**
+     * PrevPage 上一页
+     */
+    PrevPage() {
+        if (this.curPage <= 1) {
+            return;
+        }
+        this.curPage--;
+        this.GetCurPage();
+    }
+
+    /**
+     * NextPage 下一页
+     */
+    NextPage() {
+        var nPageSize = this.context.GetPageSize();
+        if (nPageSize >= this.pageSize && this.curPage >= this.pageCount) {
+            return;
+        }
+        this.curPage++;
+        this.GetCurPage();
+    }
+
+    /**
+     * LastPage 末页
+     */
+    LastPage() {
+        if (this.pageCount < 1) {
+            return;
+        }
+        this.curPage = this.pageCount;
+        this.GetCurPage();
+    }
+
+    /**
+     * TurnToPage 跳转到某页
+     * @param index 页码
+     */
+    TurnToPage(index: number) {
+        var nPageSize = this.context.GetPageSize();
+        if (index < 1 || (nPageSize >= this.pageSize && index > this.pageCount)) {
+            return;
+        }
+        this.curPage = index;
+        this.GetCurPage();
+    }
+
 }
