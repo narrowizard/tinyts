66f614c23a159f4225478e4b75665367b36ffeca
narro <501xs05@163.com>
2017-05-12 10:07:34 +0800

    InputView 增加AcceptButton

--- control/input.ts
+++ control/input.ts
@@ -1,16 +1,55 @@
 import { TextView } from './text';
+import { Button } from './button';
 
 /**
  * InputView 文本输入控件,作为输入框的基类,重载了TextView中的方法
+ * properties 
+ *      data-accept-button string jquery selector
  */
 export class InputView extends TextView {
 
-    
+    protected acceptBtn: JQuery;
+
+    LoadView(parent) {
+        var succ = super.LoadView(parent);
+        if (succ) {
+            var acceptSelector = this.target.attr("data-accept-button");
+            if (acceptSelector) {
+                this.acceptBtn = $(acceptSelector);
+            }
+            this.On("keypress", (args) => {
+                if (args.which == 13) {
+                    if (this.acceptBtn.prop("disabled")) {
+
+                    } else {
+                        this.acceptBtn.click();
+                    }
+                }
+            });
+        }
+        return succ;
+    }
+
+    /**
+     * SetAcceptButton 设置默认按钮
+     * @param id 默认按钮的选择器
+     * @param btn 默认按钮(Button对象)
+     */
+    SetAcceptButton(id: string);
+    SetAcceptButton(btn: Button);
+    SetAcceptButton(p: any) {
+        if (typeof p == "string") {
+            this.acceptBtn = $(p);
+        } else if (typeof p == "object") {
+            this.acceptBtn = (p as Button).GetJQueryInstance();
+        }
+    }
+
     Value() {
         return this.target.val();
     }
 
-    
+
     SetValue(v: string) {
         this.target.val(v);
     }
--- dist/tinyts.js
+++ dist/tinyts.js
@@ -1479,12 +1479,42 @@ System.register("tinyts/control/input", ["tinyts/control/text"], function (expor
         execute: function () {
             /**
              * InputView 文本输入控件,作为输入框的基类,重载了TextView中的方法
+             * properties
+             *      data-accept-button string jquery selector
              */
             InputView = (function (_super) {
                 __extends(InputView, _super);
                 function InputView() {
                     return _super !== null && _super.apply(this, arguments) || this;
                 }
+                InputView.prototype.LoadView = function (parent) {
+                    var _this = this;
+                    var succ = _super.prototype.LoadView.call(this, parent);
+                    if (succ) {
+                        var acceptSelector = this.target.attr("data-accept-button");
+                        if (acceptSelector) {
+                            this.acceptBtn = $(acceptSelector);
+                        }
+                        this.On("keypress", function (args) {
+                            if (args.which == 13) {
+                                if (_this.acceptBtn.prop("disabled")) {
+                                }
+                                else {
+                                    _this.acceptBtn.click();
+                                }
+                            }
+                        });
+                    }
+                    return succ;
+                };
+                InputView.prototype.SetAcceptButton = function (p) {
+                    if (typeof p == "string") {
+                        this.acceptBtn = $(p);
+                    }
+                    else if (typeof p == "object") {
+                        this.acceptBtn = p.GetJQueryInstance();
+                    }
+                };
                 InputView.prototype.Value = function () {
                     return this.target.val();
                 };
