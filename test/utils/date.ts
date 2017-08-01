503d57b4f03ad4169d1eb8293c806b7c3ff32230
narro <501xs05@163.com>
2017-08-01 13:35:42 +0800

    update mocha test

--- test/utils/date.ts
+++ test/utils/date.ts
@@ -9,11 +9,11 @@ describe('Date', function () {
       var date = TsDate.fromISO(dateString);
       assert.equal(date.date.getFullYear(), 2004);
       // month index from 0
-      assert.equal(date.date.getMonth(), 5 - 1);
-      assert.equal(date.date.getDate(), 3);
-      assert.equal(date.date.getHours(), 17);
-      assert.equal(date.date.getMinutes(), 30);
-      assert.equal(date.date.getSeconds(), 8);
+      assert.equal(date.date.getUTCMonth(), 5 - 1);
+      assert.equal(date.date.getUTCDate(), 3);
+      assert.equal(date.date.getUTCHours(), 9);
+      assert.equal(date.date.getUTCMinutes(), 30);
+      assert.equal(date.date.getUTCSeconds(), 8);
       assert.equal(date.date.getTimezoneOffset(), -8 * 60);
       assert.deepEqual(date.Format("yyyy-MM-dd hh:mm:ss"), "2004-05-03 17:30:08");
     });
